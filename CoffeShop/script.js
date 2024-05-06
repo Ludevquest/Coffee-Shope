4f57f996f76bb62c9659c048039fba3b859b14de
Ludevquest <158842997+Ludevquest@users.noreply.github.com>
2024-05-06 08:10:15 -0300

    Add files via upload

---
+++ CoffeShop/script.js
@@ -0,0 +1,5 @@
+function abrirGoogleMaps() {
+  var url = "https://www.google.com/maps/place/P%C3%A7a+Afonso+Pena,+280+-+Centro/@-23.1863969,-45.8880658,18.61z/data=!4m7!3m6!1s0x94cc4a1702288747:0xdc78afee827d2f68!4b1!8m2!3d-23.1858674!4d-45.8876687!16s%2Fg%2F11kqfy4cn3?entry=ttu";
+  
+  window.open(url, "_blank");
+}

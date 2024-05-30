7f56db7251925756039d77199a886ce5f1b9a541
Joao Alberto <joaoalbertosv15@gmail.com>
2024-05-29 21:52:43 -0300

    feat: add responsiveness and new festures

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('toggle-mode').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+  
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  const currentMode = isDarkMode ? 'Modo Claro' : 'Modo Escuro';
+  const currentModeImage = isDarkMode ? 'node_modules/bootstrap-icons/icons/sun.svg' : 'node_modules/bootstrap-icons/icons/sun-fill.svg';
+
+  this.querySelector('img').src = currentModeImage;
+  this.querySelector('img').alt = currentMode;
+});
+
+function abrirGoogleMaps() {
+  var url = "https://www.google.com/maps/place/P%C3%A7a+Afonso+Pena,+280+-+Centro/@-23.1863969,-45.8880658,18.61z/data=!4m7!3m6!1s0x94cc4a1702288747:0xdc78afee827d2f68!4b1!8m2!3d-23.1858674!4d-45.8876687!16s%2Fg%2F11kqfy4cn3?entry=ttu";
+  
+  window.open(url, "_blank");
+}
+
+function abrirFormulario() {
+  var url = "https://hasfakm2.forms.app/inscricao-coffee-shop";
+
+  window.open(url, "_blank");
+}
